b7a76fd28b5f35fb295398b331de6cf217b39f60
Christian <50722838+christianwilkins@users.noreply.github.com>
2024-09-28 12:26:54 -0400

    nav sheet WIP ui

--- App/Main/Sources/Sheet/NavigationSheet.swift
+++ App/Main/Sources/Sheet/NavigationSheet.swift
@@ -9,36 +9,57 @@ import SwiftUI
 import CoreLocation
 
 public struct NavigationSheet: View {
-    @State var coordinates: Coordinates? = nil
+    @State var poig: POI? = nil
     let location: Coordinates
     let poi: [POI]
     
     public var body: some View {
-        if let coordinates = coordinates {
-            Text("This sheet will ask if the user wants to start navigation: \(location) \n \(poi)")
-            
-            Button("Start Navigation") {
-                let url = URL(string: "maps://?saddr=&daddr=\(coordinates.latitude),\(coordinates.longitude)")
-                if let url = url, UIApplication.shared.canOpenURL(url) {
-                  UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            }
-        } else {
-            ProgressView()
-                .onAppear {
-                    var minDistance = Double.greatestFiniteMagnitude
-                    var finalCoords : Coordinates?
-                    for point in poi {
-                        let currCoords = CLLocation(latitude: point.coordinates.latitude, longitude: point.coordinates.longitude)
-                        let distance = currCoords.distance(from: CLLocation(latitude: location.latitude, longitude: location.longitude))
-                        if distance < minDistance {
-                            minDistance = distance
-                            finalCoords = point.coordinates
+        VStack {
+            if let nearestPOI = poig {
+                Text("The nearest shelter to you is: \(nearestPOI.address) \n")
+                    .onAppear {
+                        print("extra: \(nearestPOI.extra)")
+                        for item in nearestPOI.extra {
+                            print("extra: \(item)")
                         }
                     }
-                    coordinates = finalCoords
+                
+                ForEach(nearestPOI.extra, id: \.self) { item in
+                    Text(item)
                 }
+                
+                Button("Start Navigation") {
+                    let url = URL(string: "maps://?saddr=&daddr=\(nearestPOI.coordinates.latitude),\(nearestPOI.coordinates.longitude)")
+                    if let url = url, UIApplication.shared.canOpenURL(url) {
+                      UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
+                .fontWeight(.black)
+    //            .foregroundStyle(statusTextColor)
+            } else {
+                ProgressView()
+                    .onAppear {
+                        var minDistance = Double.greatestFiniteMagnitude
+                        var finalPOI : POI?
+                        for point in poi {
+                            let currCoords = CLLocation(latitude: point.coordinates.latitude, longitude: point.coordinates.longitude)
+                            let distance = currCoords.distance(from: CLLocation(latitude: location.latitude, longitude: location.longitude))
+                            if distance < minDistance {
+                                minDistance = distance
+                                finalPOI = point
+                            }
+                        }
+                        poig = finalPOI
+                    }
+            }
         }
+        .padding()
+        .background(Color.gray)
+                .cornerRadius(15)
+                .shadow(radius: 10)
+                .frame(maxWidth: 300) // Set the width of the popup
         
     }
 }
