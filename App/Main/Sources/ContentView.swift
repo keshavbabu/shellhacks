c0fb177eaa3550ed5d157fb32086132d296d587f
Keshav Babu <agent22922@protonmail.ch>
2024-09-28 03:26:57 -0400

    top statusbar

--- App/Main/Sources/ContentView.swift
+++ App/Main/Sources/ContentView.swift
@@ -13,6 +13,34 @@ public struct ContentView: View {
     
     public init() {}
     
+    var statusText: String {
+        guard let hurricane = vm.hurricaine else {
+            return ""
+        }
+        switch hurricane.hurricaine {
+        case .here:
+            return "DANGER"
+        case .incoming:
+            return "WARNING"
+        case .none:
+            return "SAFE"
+        }
+    }
+    
+    var statusTextColor: Color {
+        guard let hurricane = vm.hurricaine else {
+            return .clear
+        }
+        switch hurricane.hurricaine {
+        case .here:
+            return .red
+        case .incoming:
+            return .yellow
+        case .none:
+            return .green
+        }
+    }
+    
     public var body: some View {
         Map {
             // stuff in here will be dependant on the state
@@ -23,10 +51,26 @@ public struct ContentView: View {
         }
         .overlay(alignment: .top) {
             HStack {
-                Text("SAFE")
+                Text(statusText)
+                  .fontWeight(.black)
+                  .foregroundStyle(statusTextColor)
                   .padding(10)
                   .background(RoundedRectangle(cornerRadius: 12).fill(Color(UIColor.systemGray5)))
                   .padding()
+                Spacer()
+                if let hurricaine = vm.hurricaine {
+                    switch hurricaine.hurricaine {
+                    case .none:
+                        EmptyView()
+                    case .incoming, .here:
+                        Text("10 mi")
+                          .fontWeight(.black)
+                          .foregroundStyle(statusTextColor)
+                          .padding(10)
+                          .background(RoundedRectangle(cornerRadius: 12).fill(Color(UIColor.systemGray5)))
+                          .padding()
+                    }
+                }
             }
         }
         .onAppear {
