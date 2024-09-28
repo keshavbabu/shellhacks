b905ffb96243d2fb44f5e11d762a1bc4db8688f1
Christian <50722838+christianwilkins@users.noreply.github.com>
2024-09-28 15:51:27 -0400

    new evac view

--- App/Main/Sources/Views/EvacView.swift
+++ App/Main/Sources/Views/EvacView.swift
@@ -5,8 +5,29 @@
 //  Created by Shaheer Khan on 9/28/24.
 //
 import SwiftUI
+import MapKit
+
 struct EvacView: View {
     var body: some View {
-        Text("EvacView")
+        Map().blur(radius: 10)
+        .overlay(
+            ZStack{
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(Color(UIColor.systemGray5))
+                    .shadow(color: .black, radius: 10, x: 0, y: 10)
+                    .frame(width: 300, height: 100);
+                Text("Evacuate")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .foregroundColor(.red)
+            }
+        ).onTapGesture {
+            // keshav code
+            
+        }
+        
+//        func buttonAction(){
+//            userActivity(String, collaborating)
+//        }
     }
 }
