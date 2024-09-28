4014685cf1d2c9d15614f791e9ec7d7e653cc9fd
Shaheer Khan <119394707+shaheerkhan8476@users.noreply.github.com>
2024-09-28 17:29:34 -0400

    Map Update
    Added User Location to Map View

--- App/Main/Sources/Views/MapView.swift
+++ App/Main/Sources/Views/MapView.swift
@@ -10,12 +10,24 @@ import MapKit
 
 struct MapView: View {
     var userState: EvacuateState
+    @State private var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
+    @Namespace var mapScope
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
         span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
     )
 
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(position: $cameraPosition, scope: mapScope) {
+               }
+        .overlay(alignment: .bottomTrailing) {
+                    HStack {
+                        Spacer()
+                        MapUserLocationButton(scope: mapScope)
+                    }
+                    .padding()
+                }
+                .mapScope(mapScope)
+                
     }
 }
